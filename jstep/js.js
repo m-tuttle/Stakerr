ff486edd8429a5022ff2beb2d100a91fe22a79e2
Jeremy Stepanovich <jstepanovich91@gmail.com>
2018-03-15 11:44:50 -0500

    updated jstep files

--- jstep/js.js
+++ jstep/js.js
@@ -1,3 +1,5 @@
+// Random quote generator
+
 var query = "https://talaikis.com/api/quotes/random/";
 
 $.ajax({
@@ -8,14 +10,33 @@ $.ajax({
     $("#author").text(response.author)
 })
 
-$("#addRow").on("click", function() {
-    console.log("hi");
-    $("#feedGoals").append("<div class='row'><div class='col s3'></div><div class='card-panel col s6' style='text-align: center'>Goal Example</div><div class='col s3'></div></div>")
-});
+// Modal activation
 
 $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
   });
 
-  
+// Click functions
+
+$(document).on("click", "#follow", function() {
+
+    $(this).html("<i class='material-icons'>check</i>")
+
+})
+
+$("#shortTerm, #longTerm").on("click", function() {
+    $("#timeframe").text(this.text);
+    if(this.text === "Long Term") {
+        $("#timeframeEntry").attr("type", "date")
+    }
+    else {
+        $("#timeframeEntry").attr("type", "time")
+    }
+})
+
+// Testing append on feed
+
+// $("#addRow").on("click", function() {
+//     console.log("hi");
+//     $("#feedGoals").append("<div class='row'><div class='col s3'></div><div class='card-panel col s6' style='text-align: center'>Goal Example</div><div class='col s3'></div></div>")
+// });
