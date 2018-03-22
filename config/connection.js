96c0c9b00f88730a84a56e8d689ebb1eb7dd7d6c
Mike Tuttle <mike.s.tuttle@gmail.com>
2018-03-22 10:42:30 -0500

    added db connection for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "project_db"
-});
+if (process.env.JAWSDB_URL){
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "project_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
